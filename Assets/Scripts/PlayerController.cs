2ea771d38f13121b71af03c3ed27e89bbd803e83
KusZaJopku <Experttipo@yandex.ru>
2023-05-29 22:34:45 +0300

    2Players
    The second player. Animation. Throwing projectiles. Clearing the memory of projectiles/

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,9 +20,15 @@ public class PlayerController : MonoBehaviour
 
     public LayerMask  whatIsGround;
     public bool isGrounded;
+
+    private Animator anim;
+
+    public GameObject snowBall;
+    public Transform throwPoint;
     void Start()
     {
-      theRB = GetComponent<Rigidbody2D>();  
+      theRB = GetComponent<Rigidbody2D>();
+      anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -41,6 +47,24 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown(jump) && isGrounded)
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-        }   
+        }
+        if(Input.GetKeyDown(throwBall))
+        {
+            GameObject ballClone = (GameObject)Instantiate(snowBall, throwPoint.position, throwPoint.rotation);
+            ballClone.transform.localScale = transform.localScale;
+
+            anim.SetTrigger("Throw");
+        }
+
+        if(theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }else if(theRB.velocity.x >0)
+        {
+            transform.localScale = new Vector3(1,1,1);
+                    
+        }
+        anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("Grounded", isGrounded); 
     }
 }

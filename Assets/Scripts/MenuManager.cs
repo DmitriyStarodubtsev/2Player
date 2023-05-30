007ccf0c03399135b1d50e6b4af9c65ebbfdf3c5
KusZaJopku <Experttipo@yandex.ru>
2023-05-30 23:45:31 +0300

    Choosing Levels & Final Build

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,10 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public string mainGameScene;
-   public void StartGame()
+    public string level1;
+    public string level2;
+   public void StartLevel1()
    {
-        SceneManager.LoadScene(mainGameScene);
+        SceneManager.LoadScene(level1);
+   }
+   public void StartLevel2()
+   {
+        SceneManager.LoadScene(level2 );
    }
    public void QuitGame()
    {
